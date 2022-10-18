c345f6e35ce85fbe5f77459a5d20172df31985f6
AdnanOlarmmi <olarmmi@gmail.com>
2022-10-18 20:20:35 +0100

    Test Item Counter Function

---
+++ itemCounter.test.js
@@ -0,0 +1,8 @@
+import foodItems from "./src/modules/foodItems.js";
+import itemsCounter from "./src/modules/itemsCounter";
+
+describe('Test for Items Counter', () => {
+    test('Test Items counter', () => {
+        expect(itemsCounter()).toBe(foodItems.length);
+    })
+})
