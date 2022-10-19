921136afce8a77bbe2132341b87649053a3043e2
AdnanOlarmmi <olarmmi@gmail.com>
2022-10-19 10:04:45 +0100

    Ui for mobile ultimately responsive

--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
 import './styles.css';
-import renderFoodItems from './modules/renderFoodItems.js';
 import renderLikes from './modules/renderLikes.js';
 import itemsCounter from './modules/itemsCounter.js';
-
 import addLikes from './modules/addLikes.js';
+import renderFoodItems from './modules/renderFoodItems.js';
 
 const foodContainerEl = document.querySelector('main');
 const hamburgerEl = document.querySelector('.hamburger');
 const navEl = document.querySelector('nav');
 const barsEl = document.getElementsByClassName('bar');
+const itemsNumberEl = document.querySelector('.item_number');
+
+itemsNumberEl.innerHTML = itemsCounter();
+
 const toggleNav = () => {
     hamburgerEl.addEventListener('click', () => {
         navEl.classList.toggle('open');
         for(let item of barsEl) {
             item.classList.toggle('change');
         }
+        foodContainerEl.classList.toggle('shift');
     })
 }
 
-
-const itemsNumberEl = document.querySelector('.item_number');
-
-itemsNumberEl.innerHTML = itemsCounter();
-
-const runRenderLikes = async () => {
-  await renderFoodItems();
-  renderLikes();
-};
-runRenderLikes();
 foodContainerEl.addEventListener('click', async (e) => {
   if (e.target.className === 'icon__like') {
     await addLikes(e.target.parentNode.id);
@@ -35,6 +29,8 @@ foodContainerEl.addEventListener('click', async (e) => {
   }
 });
 
+renderLikes();
+renderFoodItems();
 toggleNav();
 
 
