aaa937141a38cddbea6933c2f727c30a10747baa
AdnanOlarmmi <olarmmi@gmail.com>
2022-10-22 12:28:06 +0100

    Remove console statement

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import likeIcon from './images/likeIcon.svg'
 import foodBaseUrl from './modules/config.js';
 import renderFoodItems from './modules/renderFoodItems.js'
 
-console.log(foodBaseUrl);
+
+
 renderFoodItems();
 
 document.querySelector('.sam').src = likeIcon;
